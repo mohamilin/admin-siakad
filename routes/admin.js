700210ddb710dbb9b309528475e6fd99987b67be
Moh Amilin <amilinmoh99@gmail.com>
2021-03-13 20:49:21 +0700

    add : route

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {viewDashboard} = require("../controllers/admin")
+const {viewDashboard} = require("../controllers/dashboard")
+const {viewLembaga} = require("../controllers/lembaga")
+const {viewTahunPelajaran} = require("../controllers/tahunPelajaran")
+const {viewGuru} = require("../controllers/guru")
+const {viewKelas} = require("../controllers/kelas")
+const {viewMatePelajaran} = require("../controllers/mapel")
+
 
 router.get('/dashboard', viewDashboard)
+router.get('/lembaga', viewLembaga)
+router.get('/tahun-pelajaran', viewTahunPelajaran)
+router.get('/guru', viewGuru)
+router.get('/kelas', viewKelas)
+router.get('/mapel', viewMatePelajaran)
+
+
 
 module.exports = router;
